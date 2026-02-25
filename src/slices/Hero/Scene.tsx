649c7a0ff69ad0ec84992cc04e7c7aa86a1a8357
Christopher Ayodele <ayodelebabajidechris123@gmail.com>
2026-02-25 14:43:41 +0100

    4: Add Keycap Model

--- src/slices/Hero/Scene.tsx
+++ src/slices/Hero/Scene.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Keyboard } from "@/components/Keyboard";
+import { Keycap } from "@/components/Keycap";
 import { Environment, PerspectiveCamera } from "@react-three/drei";
 import { useControls } from "leva";
 
@@ -15,17 +16,42 @@ export function Scene() {
       rotationZ: 0,
     });
 
+  const scalingFactor = window.innerWidth <= 500 ? 0.5 : 1;
+
   return (
     <group>
       <PerspectiveCamera makeDefault position={[0, 0, 4]} fov={50} />
       {/*rotation is by radiants */}
-      <Keyboard
-        scale={9}
-        position={[0.2, -0.5, 1.9]}
-        rotation={[1.6, 0.4, 0]}
-      />
-      {/* <ambientLight intensity={0.2} /> */}
 
+      <group scale={scalingFactor}>
+        <Keyboard
+          scale={9}
+          position={[0.2, -0.5, 1.9]}
+          rotation={[1.6, 0.4, 0]}
+        />
+        {/* <ambientLight intensity={0.2} /> */}
+
+        <group>
+          <Keycap position={[0, -0.4, 2.6]} rotation={[0, 2, 3]} texture={0} />
+          <Keycap position={[-1.4, 0, 2.3]} rotation={[3, 2, 1]} texture={1} />
+          <Keycap position={[-1.8, 1, 1.5]} rotation={[0, 1, 3]} texture={2} />
+          <Keycap position={[0, 1, 1]} rotation={[0, 4, 2]} texture={3} />
+          <Keycap position={[0.7, 0.9, 1.4]} rotation={[3, 2, 0]} texture={4} />
+          <Keycap
+            position={[1.3, -0.3, 2.3]}
+            rotation={[1, 2, 0]}
+            texture={5}
+          />
+          <Keycap position={[0, 1, 2]} rotation={[2, 2, 3]} texture={6} />
+          <Keycap position={[-0.7, 0.6, 2]} rotation={[1, 4, 0]} texture={7} />
+          <Keycap
+            position={[-0.77, 0.1, 2.8]}
+            rotation={[3, 2, 3]}
+            texture={8}
+          />
+          <Keycap position={[2, 0, 1]} rotation={[0, 0, 3]} texture={0} />
+        </group>
+      </group>
       <Environment
         files={["/hdr/blue-studio.hdr"]}
         environmentIntensity={0.05}
